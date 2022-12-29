dc51ad30974687ed6abd7f693aa7e8b1000aee84
studentqa2023 <sarowerhome@gmail.com>
2022-12-29 12:50:11 -0500

    Human class is added

---
+++ src/main/java/com/practice/Human.java
@@ -0,0 +1,14 @@
+package com.practice;
+
+public class Human {
+	
+	public void getMan() {
+		System.out.println("I am a human.");
+	}
+	
+	public static void main(String[] args) {
+		Human obj = new Human();
+		obj.getMan();
+	}
+
+}
